a71bb31e10647bc2d565002b4586027f22f0d7a1
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-09 22:02:31 +0200

    Added the main section

--- Travel Journal/src/components/Main.jsx
+++ Travel Journal/src/components/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <div className="main">
+      <div className="main__container">
+      <h1 className="main__container-title">The Great Outdoors</h1>
+      <p className="main__container-quote">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      </p>
+      <p className="main__container-author">— Various Artist</p>
+      </div>
+      <div className="main__button"></div>
+    </div>
+  );
 }
