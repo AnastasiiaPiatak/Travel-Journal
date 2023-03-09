41124849108799c971d7ef69d83236ac72d40f66
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-09 21:31:07 +0200

    Created the header template

--- Travel Journal/src/components/Header.jsx
+++ Travel Journal/src/components/Header.jsx
@@ -1,5 +1,13 @@
 export default function Header(){
     return(
-        <h1>Header</h1>
+        <div className="header">
+            <nav className="header__nav">
+                <ul>
+                    <li><a href="/">EXPLORE</a></li>
+                    <li><a href="/">HOME</a></li>
+                    <li><a href="/">JOURNAL</a></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
