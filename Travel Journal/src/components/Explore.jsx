95cca6198d7b3ce58e3e0e9dc02832fa3abbdccf
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-10 01:33:29 +0200

    Added the explore section's template

--- Travel Journal/src/components/Explore.jsx
+++ Travel Journal/src/components/Explore.jsx
@@ -1,3 +1,32 @@
 export default function Explore() {
-  return <h1>Explore</h1>;
+  return (
+    <div className="explore">
+      <div className="explore__text">
+        <h2 className="explore__text-title">Explore the World</h2>
+        <p className="explore__text-text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae qui
+          cum vitae architecto et eveniet. Porro reiciendis optio enim quia
+          cupiditate beatae corrupti harum maiores, tempora, aspernatur maxime.
+          Totam, perferendis?
+        </p>
+      </div>
+      <div className="explore__gallery">
+        <div className="explore__gallery-card">
+          <img src="" alt="" />
+          <h4 className="explore__galery-cardTitle"></h4>
+          <p className="explore__galery-cardText"></p>
+        </div>
+        <div className="explore__gallery-card">
+          <img src="" alt="" />
+          <h4 className="explore__galery-cardTitle"></h4>
+          <p className="explore__galery-cardText"></p>
+        </div>
+        <div className="explore__gallery-card">
+          <img src="" alt="" />
+          <h4 className="explore__galery-cardTitle"></h4>
+          <p className="explore__galery-cardText"></p>
+        </div>
+      </div>
+    </div>
+  );
 }
