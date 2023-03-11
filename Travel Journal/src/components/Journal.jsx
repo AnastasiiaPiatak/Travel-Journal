0d9f2b0b8d51238aa7429d680748954f7e19f0a0
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-11 02:04:35 +0200

    Added journal section's html template

--- Travel Journal/src/components/Journal.jsx
+++ Travel Journal/src/components/Journal.jsx
@@ -1,3 +1,47 @@
 export default function Journal() {
-  return <h1>Journal</h1>;
+  return (
+    <div className="journal">
+      <div className="journal__text">
+        <div className="journal__text-title">The Journal</div>
+        <div className="journal__text-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          alias voluptatem dignissimos aspernatur quisquam dolorum, architecto
+          fugiat facere quidem voluptatibus fugit, eveniet perspiciatis quasi
+          libero molestiae! Dicta illum adipisci aut.
+        </div>
+      </div>
+      <div className="journal__gallery">
+        <div className="journal__gallery-card">
+          <img src="" alt="" />
+          <div className="journal__gallery-text">
+            <p className="journal__gallery-textData">02.11.2022</p>
+            <h4 className="journal__gallery-textTitle">
+              The interesting place
+            </h4>
+            <p className="journal__gallery-textInfo">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
+              maiores, possimus ipsum voluptates accusantium cum libero rerum
+              provident suscipit. Eius ipsa explicabo praesentium doloribus
+              aliquam quia unde, mollitia ducimus quos.
+            </p>
+          </div>
+        </div>
+        <div className="journal__gallery-card">
+          <img src="" alt="" />
+          <div className="journal__gallery-text">
+            <p className="journal__gallery-textData">27.08.2020</p>
+            <h4 className="journal__gallery-textTitle">
+              The interesting place
+            </h4>
+            <p className="journal__gallery-textInfo">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
+              maiores, possimus ipsum voluptates accusantium cum libero rerum
+              provident suscipit. Eius ipsa explicabo praesentium doloribus
+              aliquam quia unde, mollitia ducimus quos.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
